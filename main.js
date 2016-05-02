95f2d4d72cf7e78ca1c7750c45d00d332572ce8b
Michelle Aphaiyarath <maphaiyarath@gmail.com>
2016-05-02 10:03:06 -0500

    Added files via upload

--- main.js
+++ main.js
@@ -1,111 +1,7 @@
+// alert('Welcome to Michelle and Jacquie\'s data viz about crime at SMU!');
 
-
-function CrimeLog(type, reportNumber, date, time, location, status) {
-	this.type = type;
-	this.reportNumber = +reportNumber;
-	this.date = +date;
-	this.time = time; 
-	this.location = location;
-	this.status = status;
-
-};
-
-CrimeLog.prototype.getType = function() {
-	return this.type; 
-};
-
-CrimeLog.prototype.getDate = function() {
-	return this.date; 
-};
-
-CrimeLog.prototype.getLocaton = function() {
-	return this.location;
-};
-
-CrimeLog.prototype.getTime = function() {
-	return this.time;
-};
-
-CrimeLog.prototype.getStatus = function() {
-	return this.status;
-};
-
-CrimeLog.prototype.getReportNumber = function() {
-	return this.reportNumber; 
-};
-
-CrimeLog.prototype.print = function() {
-	console.log(this.type, this.date, this.time, this.location);
-}
-
-function Parser() {
-	this.url = "http://www.smu.edu/BusinessFinance/Police/CrimeLog/SMU%20-%20Main%20Campus/";
-	this.document = "records.csv"; 
-};
-
-Parser.prototype.sortChronological = function() {
-	
-};
-
-Parser.prototype.getNumOfRows = function() {
-
-};
-
-Parser.prototype.run = function() {
-	this.scrapeTable();
-};
-
-
-Parser.prototype.cleanIncident = function(incident) {
-	var numOfIncidents = 0;
-	var startIndex = 0; 
-	for(var i = 0; i < incident.length; i++){
-		if(incident.charAt(i) == ',') numOfIncidents++;
-	}
-	var incidents = [];
-	if(numOfIncidents>0) {
-		for(var i = 0; i < numOfIncidents; i++) {
-			endIndex = incident.indexOf(',', startIndex); 
-			if(endIndex == -1) endIndex=temp.lenth();
-			incidents[i] = incident.substring(startInde, endIndex);
-			startIndex = endIndex;
-		}
-	}
-	return incidents; 
-};
-
-Parser.prototype.cleanDate = function(date) {
-	return date;  
-};
-
-Parser.prototype.cleanReportNumber = function(id) {
-	return id;
-};
-
-Parser.prototype.cleanLocation = function(location) {
-	return location; 
-};
-
-Parser.prototype.parse = function() {
-	var parser = this;
-	d3.csv(this.document, function(data) { 
-		numOfCrimeLogs++;
-		data.forEach(function(d) {
-			d.incident = parser.cleanIncident(d.incident);
-			//var incident = this.cleanIncident(d.incident);
-			//var date = this.cleanDate(d.date);
-		});
-		console.log(data);
-		//return {key: d.key, value: d.value}; 
-	});
-};
-
-var crime = new CrimeLog("Sally", "1", "2/3", 1100, "Moody", "closed");
-var crimeLogs = [];
-var parser = new Parser(); 
-var numOfCrimeLogs = 0; 
-parser.parse();
-
+/*
+// test graphic
 d3.select('#viz')
 	.append('svg')
 		.attr('width', 600)
@@ -118,21 +14,182 @@ d3.select('#viz')
 		.attr('height', 200)
 		.style('fill', '#CB4321')
 	d3.select('svg')
-		.append('circle')
+		.append("circle")
 		.attr('cx', 200)
 		.attr('cy', 200)
 		.attr('r', 100)
 		.style('fill', '#840042')
+*/
+
+// =============================================
+
+// test graphic w/ irrelevant data
+// view array of objects in console
+d3.csv("records.csv", function(d) {
+	return {
+		incident: d.Incident,
+		report_number: +d["Report Number"],
+		date_reported: d["Date Reported"],
+		date_occurred: d["Date Occurred"],
+		location: d.Location,
+		disposition: d.Disposition
+	};
+}, function(data) {
+	console.log(data);
+	// alert ('Data loaded!');
+});
+
+// irrelevant data
+var bardata = [10, 22, 7, 8, 2, 2, 3, 4, 3, 8, 13, 12, 7, 6, 9, 6, 14, 7, 8, 5, 8, 11, 12, 9];
+
 
 /*
-var chart = d3.select('.chart')
-	.attr({
-		'width': totalWidth;
-		'height': totalHeight;
-	})
-;
-var bar = chart.append('g');
-bar.append('rect')
-	.attr('width', 20);
-	.attr('height', crime.time/10);
+bardata.sort(function compareNumbers(a, b) {
+	return a - b;
+});
 */
+
+var margin = {top: 30, right:30, bottom: 40, left:50}
+
+// variables for graphic
+var height = 400 - margin.top - margin.bottom,
+	width = 800 - margin.left - margin.right,
+	barWidth = 50;
+	barOffset = 5;
+
+// gradient color scale
+// perhaps have legend where dif type of crime is dif color?
+var colors = d3.scale.linear()
+	.domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
+	.range(['#FFB832', '#C61C6F', '#268BD2', '#85992C'])
+
+var tempColor;
+
+// map data to canvas height
+var yScale = d3.scale.linear()
+	.domain([0, d3.max(bardata)])
+	.range([0, height])
+
+// map data to canvas width
+var xScale = d3.scale.ordinal()
+	.domain(d3.range(0, bardata.length))
+	.rangeBands([0, width], 0.2)
+
+var tooltip = d3.select('body').append('div')
+	.style('position', 'absolute')
+	.style('padding', '0 10px')
+	.style('background', 'gray')
+	.style ('opacity', .2)
+
+// start drawing test graphic
+var myChart = d3.select('#viz2').append('svg')
+	.style ('background', '#E7E0CB')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
+	.append('g')
+	.attr('transform', 'translate(' + margin.left +', '+margin.top+')')
+	.selectAll('rect').data(bardata)
+	.enter().append('rect')
+		.style('fill', function(d, i) {
+			return colors(i);
+		})
+		.attr('width', xScale.rangeBand())
+		.attr('x', function(d, i) {
+			return xScale(i);
+		})
+		.attr('height', 0)
+		.attr('y', height)
+
+	// user interactive
+	.on('mouseover', function(d) {
+		tooltip.transition()
+			.style('opacity', .9)
+
+		// show value of bar
+		tooltip.html(d)
+			.style('left', (d3.event.pageX - 20) + 'px')
+			.style('top', (d3.event.pageY - 30) + 'px')
+
+		tempColor = this.style.fill;
+		d3.select(this)
+			.style('opacity', .5)
+			.style('fill', 'blue')
+	})
+	.on('mouseout', function(d) {
+		d3.select(this)
+			.style('opacity', 1)
+			.style('fill', tempColor)
+	})
+
+// animation
+myChart.transition()
+	.attr('height', function(d) {
+		return yScale(d);
+	})
+	.attr('y', function(d) {
+		return height - yScale(d);
+	})
+	.delay(function(d, i) {
+		return i * 10;
+	})
+	.duration(1000)
+	.ease('elastic')
+
+// y axis
+var vGuideScale = d3.scale.linear()
+	.domain([0, d3.max(bardata)])
+	.range([height, 0])
+
+var vAxis = d3.svg.axis()
+	.scale(vGuideScale)
+	.orient('left')
+	.ticks(10)
+
+var vGuide = d3.select('svg').append('g')
+	vAxis(vGuide)
+	vGuide.attr('transform', 'translate('+margin.left+', '+margin.top+')')
+	vGuide.selectAll('path')
+		.style({fill: 'none', stroke: "#000"})
+	vGuide.selectAll('line')
+		.style({stroke: "#000"})
+
+/*
+// x axis
+var hAxis = d3.svg.axis()
+	.scale(xScale)
+	.orient('bottom')
+	// 5 tick marks
+	.tickValues(xScale.domain().filter(function(d, i) {
+		return !(i % (bardata.length/5));
+	}))
+
+var hGuide = d3.select('svg').append('g')
+	hAxis(hGuide)
+	hGuide.attr('transform', 'translate('+margin.left+', '+(height+margin.top)+')')
+	hGuide.attr('transform', 'translate(0, ' + (height - 30) + ')')
+	hGuide.selectAll('path')
+		.style({fill: 'none', stroke: "#000"})
+	hGuide.selectAll('line')
+		.style({stroke: "#000"})
+*/
+
+
+
+
+
+
+
+
+
+
+// open -a 'Google Chrome' --args -allow-file-access-from-files
+
+
+
+
+
+
+
+
+
+
